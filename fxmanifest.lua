833374519f781293e03fa288cc3de277cb1a8b1f
Pinkablee <101295543+Pinkablee@users.noreply.github.com>
2023-10-25 17:16:00 -0700

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ lua54 'yes'
 
 author 'Pinkable'
 version '1.0.0'
-description '(p-blocks) A modern, well-rounded FiveM gang block system.'
+description '(p-blocks), A contemporary and versatile gang block system for FiveM.'
 
 shared_script 'config.lua'
 client_script 'client/client.lua'
